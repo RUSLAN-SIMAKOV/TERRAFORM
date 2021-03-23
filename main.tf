cf6e67fd8a249a92282d95aa5526079ddb03c6f6
RUSLAN SIMAKOV <ua667766706@gmail.com>
2021-03-23 23:31:40 +0200

    Created custom Route Table

--- main.tf
+++ main.tf
@@ -19,7 +19,25 @@ resource "aws_internet_gateway" "gw" {
     Name = "prod-gateway"
   }
 }
+
 //3. Create custom Route Table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.quantum2.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod-rt"
+  }
+}
 //4. Create Subnet
 //5. Associate Subnet with Route Table
 //6. Create Security group to allow ports 22, 80, 443
